e871c4fa6d58b4518372b96639f02cb479888007
Kim Lofthouse <kimmiikal@gmail.com>
2016-10-19 18:12:32 +0100

    common.php
    db name amended

--- common.php
+++ common.php
@@ -4,4 +4,4 @@ if(!isset($_SESSION)) {
   session_start();
 }
 
-$gPDO = new PDO('mysql://host=127.0.0.1;dbname=creepy_blog', 'root', '');
+$gPDO = new PDO('mysql://host=127.0.0.1;dbname=blog', 'root', '');
