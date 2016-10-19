26ca0403f0fcab04d6a114701425ba16d7643655
Kim Lofthouse <kimmiikal@gmail.com>
2016-10-19 18:21:27 +0100

    functions.php
    Updated to try and fix a fatal error.

--- functions.php
+++ functions.php
@@ -2,13 +2,16 @@
 
 include 'common.php';
 
+$password = $_POST['password'];
+
 if(!empty($_POST)){
   $checkemail = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+}
 
-function create_user($pdo, $email, $password) {
-    $secure_password = password_hash($password, PASSWORD_DEFAULT);
-    $prepQ = $pdo->prepare('INSERT INTO users (email, password) VALUES(?, ?)');
-    return $prepQ->execute([$email, $password]);
+function create_user($pdo, $fullname, $username, $email, $password) {
+    $password = password_hash($password, PASSWORD_DEFAULT);
+    $prepQ = $pdo->prepare('INSERT INTO users (fullname, username, email, password) VALUES(?, ?, ?, ?)');
+    return $prepQ->execute([$fullname, $username, $email, $password]);
 }
 
 function get_user($pdo, $email) {
@@ -18,7 +21,7 @@ function get_user($pdo, $email) {
     return $prepQ->fetch(PDO::FETCH_ASSOC);
 }
 
-function login($pdo, $email, $password) {
+function login($pdo, $username, $email, $password) {
     $user = get_user($pdo, $email);
 
     if(!empty($user) && password_verify($password, $user['password'])) {
