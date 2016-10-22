3b9097987093883c3eb930f37386111d13d38aa6
Kim Lofthouse <kimmiikal@gmail.com>
2016-10-22 19:35:59 +0100

    admin.php

--- admin.php
+++ admin.php
@@ -1,30 +1,38 @@
 <?php
+
 include 'common.php';
-include 'functions.php';
 
-if(!empty($_SESSION)) {
-    header('Location: /admin.php');
-    die();
+ensure_login();
+
+if (empty($_GET) || empty($_GET['action'])) {
+    display_page('admin', [
+    'articles' => $gPDO->query('SELECT * FROM articles')->fetchAll(),
+    'users' => $gPDO->query('SELECT * FROM users')->fetchAll(),
+  ]);
+
+    exit();
 }
 
-if(!empty($_POST)) {
-    insert_article($_POST['body'], $_SESSION['id']);
-    header('Location: /index.php');
+$nusql = 'INSERT INTO users(username, password, role) VALUES(:username, :password, :role)';
+$dusql = 'DELETE FROM users WHERE id = :id';
+$dasql = 'DELETE FROM articles WHERE id = :id';
+
+switch ($_GET['action']) {
+  case 'newuser':
+    $stmt = $gPDO->prepare($nusql);
+    $stmt->execute(['password' => password_hash($_POST['password'], PASSWORD_DEFAULT)] + $_POST);
+  break;
+
+  case 'deluser':
+    $stmt = $gPDO->prepare($dusql);
+    $stmt->execute(['id' => $_GET['id']]);
+  break;
+
+  case 'delarticle';
+    $stmt = $gPDO->prepare($dasql);
+    $stmt->execute(['id' => $_GET['id']]);
+  break;
+
+  default:
+    throw new Exception('Unknown Action!');
 }
-
-?>
-<html>
-<head><title>Author Admin</title></head>
-<body>
-    <h1>Story Admin</h1>
-    <h3>Add a new story to Creepy Courgetti!</h3>
-    <form action="" method="POST">
-      <label for="title">Title:</label>
-        <input name="title" type="text">
-      <label for="story">Story:</label>
-        <textarea name="story" rows="20" cols="20"></textarea>
-        <button type="submit">Publish!</button>
-    </form>
-
-</body>
-</html>
